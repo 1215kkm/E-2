9df1ecc6776b48a38060abc79d6a31405641462c
1215kkm <66404083+1215kkm@users.noreply.github.com>
2024-06-14 18:31:10 +0900

    웹디자인기능사 E-2

---
+++ script/action.js
@@ -0,0 +1,17 @@
+/* slide */
+setInterval(function(){
+    $('.slide ul').animate({top:-900}, function(){
+        $('.slide li').eq(0).appendTo($('.slide ul'))
+        $('.slide ul').css({top:0})
+    })
+}, 2000)
+
+
+/* popup */
+$('.notice li').eq(0).click(function(){
+    $('.modal').slideDown()
+})
+
+$('.popup a').click(function(){
+    $('.modal').slideUp();
+})
